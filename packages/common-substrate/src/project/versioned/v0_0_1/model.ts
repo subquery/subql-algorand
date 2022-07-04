56bb4587cbe327e409bc3bfd3679a90699767737
nguyentuan7277 <tuan97.gs19@gmail.com>
2022-07-04 10:34:58 +0700

    change version algorand

--- packages/common-substrate/src/project/versioned/v0_0_1/model.ts
+++ packages/common-substrate/src/project/versioned/v0_0_1/model.ts
@@ -1,86 +1,130 @@
-// Copyright 2020-2022 OnFinality Limited authors & contributors
+// Copyright 2020-2021 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import {ProjectManifestBaseImpl} from '@subql/common';
-import {AlgorandDataSourceKind, AlgorandHandlerKind, AlgorandNetworkFilter, AlgorandRuntimeHandler} from '@subql/types';
-import {plainToClass, Transform, Type} from 'class-transformer';
-import {
-  Equals,
-  IsArray,
-  IsEnum,
-  IsInt,
-  IsObject,
-  IsOptional,
-  IsString,
-  ValidateNested,
-  validateSync,
-} from 'class-validator';
-import {ChainTypes, SubqlNetworkFilterImpl, BlockHandler} from '../../models';
-import {AlgorandProjectNetworkConfig} from '../../types';
-import {ManifestV0_0_1Mapping, ProjectManifestV0_0_1, RuntimeDataSourceV0_0_1} from './types';
+import {BaseMapping, ProjectManifestBaseImpl} from '@subql/common';
+import {AlgorandNetworkFilter} from '@subql/common-substrate';
+import {AlgorandCustomDataSource} from '@subql/types';
+import {plainToClass, Type} from 'class-transformer';
+import {Equals, IsArray, IsObject, IsOptional, IsString, ValidateNested, validateSync} from 'class-validator';
+import yaml from 'js-yaml';
+import {CustomDataSourceBase, RuntimeDataSourceBase} from '../../models';
+import {CustomDataSourceV0_0_1, ProjectManifestV0_0_1, RuntimeDataSourceV0_0_1} from './types';
 
-export class ProjectNetworkV0_0_1 extends ChainTypes implements AlgorandProjectNetworkConfig {
+export class FileType {
+  @IsString()
+  file: string;
+}
+
+export class ProjectNetworkDeploymentV0_0_1 {
+  @IsString()
+  chainId: string;
+}
+
+export class ProjectNetworkV0_0_1 extends ProjectNetworkDeploymentV0_0_1 {
   @IsString()
   endpoint: string;
   @IsString()
   @IsOptional()
   dictionary?: string;
-}
-
-export class RuntimeMappingV0_0_1 implements ManifestV0_0_1Mapping {
-  @Transform((params) => {
-    const handlers: AlgorandRuntimeHandler[] = params.value;
-    return handlers.map((handler) => {
-      switch (handler.kind) {
-        case AlgorandHandlerKind.Block:
-          return plainToClass(BlockHandler, handler);
-        default:
-          throw new Error(`handler ${(handler as any).kind} not supported`);
-      }
-    });
-  })
-  @IsArray()
-  @ValidateNested()
-  handlers: AlgorandRuntimeHandler[];
-}
-
-export class RuntimeDataSourceV0_0_1Impl implements RuntimeDataSourceV0_0_1 {
   @IsString()
-  name: string;
-  @IsEnum(AlgorandDataSourceKind, {groups: [AlgorandDataSourceKind.Runtime]})
-  kind: AlgorandDataSourceKind.Runtime;
-  @Type(() => RuntimeMappingV0_0_1)
-  @ValidateNested()
-  mapping: RuntimeMappingV0_0_1;
   @IsOptional()
-  @IsInt()
-  startBlock?: number;
-  @IsOptional()
-  @ValidateNested()
-  @Type(() => SubqlNetworkFilterImpl)
-  filter?: AlgorandNetworkFilter;
+  genesisHash?: string;
 }
 
-export class ProjectManifestV0_0_1Impl extends ProjectManifestBaseImpl<null> implements ProjectManifestV0_0_1 {
+function validateObject(object: any, errorMessage = 'failed to validate object.'): void {
+  const errors = validateSync(object, {whitelist: true, forbidNonWhitelisted: true});
+  if (errors?.length) {
+    // TODO: print error details
+    const errorMsgs = errors.map((e) => e.toString()).join('\n');
+    throw new Error(`${errorMessage}\n${errorMsgs}`);
+  }
+}
+
+export class AlgorandRuntimeDataSourceV0_0_1Impl extends RuntimeDataSourceBase implements RuntimeDataSourceV0_0_1 {
+  validate(): void {
+    return validateObject(this, 'failed to validate runtime datasource.');
+  }
+}
+
+export class AlgorandCustomDataSourceV0_0_1Impl<
+    K extends string = string,
+    T extends AlgorandNetworkFilter = AlgorandNetworkFilter,
+    M extends BaseMapping<any, any> = BaseMapping<Record<string, unknown>, any>
+  >
+  extends CustomDataSourceBase<K, T, M>
+  implements AlgorandCustomDataSource<K, T, M>
+{
+  validate(): void {
+    return validateObject(this, 'failed to validate custom datasource.');
+  }
+}
+
+export class DeploymentV0_0_1 {
   @Equals('0.0.1')
+  @IsString()
   specVersion: string;
   @ValidateNested()
-  @Type(() => ProjectNetworkV0_0_1)
-  @IsObject()
-  network: ProjectNetworkV0_0_1;
-  @IsString()
-  schema: string;
+  @Type(() => FileType)
+  schema: FileType;
   @IsArray()
   @ValidateNested()
-  @Type(() => RuntimeDataSourceV0_0_1Impl)
-  dataSources: RuntimeDataSourceV0_0_1[];
+  @Type(() => AlgorandCustomDataSourceV0_0_1Impl, {
+    discriminator: {
+      property: 'kind',
+      subTypes: [{value: AlgorandRuntimeDataSourceV0_0_1Impl, name: 'algorand/Runtime'}],
+    },
+    keepDiscriminatorProperty: true,
+  })
+  dataSources: (RuntimeDataSourceV0_0_1 | CustomDataSourceV0_0_1)[];
+  @ValidateNested()
+  @Type(() => ProjectNetworkDeploymentV0_0_1)
+  network: ProjectNetworkDeploymentV0_0_1;
+}
 
-  get deployment(): null {
-    throw new Error('Manifest spec 0.0.1 is not support for deployment, please migrate to 0.2.0 or above');
+export class ProjectManifestV0_0_1Impl
+  extends ProjectManifestBaseImpl<DeploymentV0_0_1>
+  implements ProjectManifestV0_0_1
+{
+  @Equals('0.0.1')
+  specVersion: string;
+  @IsString()
+  name: string;
+  @IsString()
+  version: string;
+  @IsObject()
+  @ValidateNested()
+  @Type(() => ProjectNetworkV0_0_1)
+  network: ProjectNetworkV0_0_1;
+  schema: FileType;
+  @IsArray()
+  @ValidateNested()
+  @Type(() => AlgorandCustomDataSourceV0_0_1Impl, {
+    discriminator: {
+      property: 'kind',
+      subTypes: [{value: AlgorandRuntimeDataSourceV0_0_1Impl, name: 'algorand/Runtime'}],
+    },
+    keepDiscriminatorProperty: true,
+  })
+  dataSources: (RuntimeDataSourceV0_0_1 | CustomDataSourceV0_0_1)[];
+  private _deployment: DeploymentV0_0_1;
+
+  toDeployment(): string {
+    return yaml.dump(this._deployment, {
+      sortKeys: true,
+      condenseFlow: true,
+    });
+  }
+
+  get deployment(): DeploymentV0_0_1 {
+    if (!this._deployment) {
+      this._deployment = plainToClass(DeploymentV0_0_1, this);
+      validateSync(this._deployment, {whitelist: true});
+    }
+    return this._deployment;
   }
 
   validate(): void {
-    const errors = validateSync(this, {whitelist: true, forbidNonWhitelisted: true});
+    const errors = validateSync(this.deployment, {whitelist: true, forbidNonWhitelisted: true});
     if (errors?.length) {
       // TODO: print error details
       const errorMsgs = errors.map((e) => e.toString()).join('\n');
--- packages/common-substrate/src/project/versioned/v0_0_1/types.ts
+++ packages/common-substrate/src/project/versioned/v0_0_1/types.ts
@@ -1,34 +1,41 @@
-// Copyright 2020-2022 OnFinality Limited authors & contributors
+// Copyright 2020-2021 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import {RegisteredTypes} from '@polkadot/types/types';
-import {BaseMapping, IProjectManifest} from '@subql/common';
 import {
-  AlgorandRuntimeDataSource,
-  AlgorandNetworkFilter,
-  AlgorandRuntimeHandlerFilter,
-  AlgorandRuntimeHandler,
+  AlgorandCustomDataSource,
+  AlgorandCustomHandler,
+  AlgorandDataSource,
   AlgorandDataSourceKind,
+  AlgorandHandler,
+  AlgorandMapping,
+  AlgorandRuntimeDataSource,
+  AlgorandRuntimeHandler,
 } from '@subql/types';
-import {AlgorandProjectNetworkConfig} from '../../types';
+import {IAlgorandProjectManifest} from '../../types';
 
-export type ProjectNetworkConfigV0_0_1 = AlgorandProjectNetworkConfig & RegisteredTypes;
-
-// export interface RuntimeDataSourceV0_0_1 extends AlgorandRuntimeDataSource {
-//   name: string;
-//   filter?: AlgorandNetworkFilter;
-// }
-
-export type ManifestV0_0_1Mapping = Omit<BaseMapping<AlgorandRuntimeHandlerFilter, AlgorandRuntimeHandler>, 'file'>;
-
-export interface RuntimeDataSourceV0_0_1 extends Omit<AlgorandRuntimeDataSource, 'mapping'> {
-  name: string;
-  filter?: AlgorandNetworkFilter;
-  kind: AlgorandDataSourceKind.Runtime;
-  mapping: ManifestV0_0_1Mapping;
+export interface SubqlMappingV0_0_1<T extends AlgorandHandler> extends AlgorandMapping<T> {
+  file: string;
 }
 
-export interface ProjectManifestV0_0_1 extends IProjectManifest<RuntimeDataSourceV0_0_1> {
-  schema: string;
-  network: ProjectNetworkConfigV0_0_1;
+export type RuntimeDataSourceV0_0_1 = AlgorandRuntimeDataSource;
+export type CustomDataSourceV0_0_1 = AlgorandCustomDataSource;
+
+export interface ProjectManifestV0_0_1 extends IAlgorandProjectManifest {
+  name: string;
+  version: string;
+  schema: {
+    file: string;
+  };
+
+  network: {
+    endpoint?: string;
+    dictionary?: string;
+    chainId: string;
+  };
+
+  dataSources: (RuntimeDataSourceV0_0_1 | CustomDataSourceV0_0_1)[];
+}
+
+export function isRuntimeDataSourceV0_0_1(dataSource: AlgorandDataSource): dataSource is RuntimeDataSourceV0_0_1 {
+  return dataSource.kind === AlgorandDataSourceKind.Runtime && !!(dataSource as RuntimeDataSourceV0_0_1).mapping.file;
 }
