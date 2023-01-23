674874234ec3ba63da38518af429b9c2f33b9f99
Scott Twiname <skott.twiname@gmail.com>
2023-01-24 09:24:46 +1300

    Update node core

--- packages/node/src/configure/SubqueryProject.ts
+++ packages/node/src/configure/SubqueryProject.ts
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Injectable } from '@nestjs/common';
-import { RegisteredTypes } from '@polkadot/types/types';
 import {
   ReaderFactory,
   ReaderOptions,
