fc878a203334058f58fd2a830d15e770fd40c3a1
Scott Twiname <skott.twiname@gmail.com>
2024-12-12 10:09:07 +1300

    Add getBlockSize function

--- packages/node/src/indexer/types.ts
+++ packages/node/src/indexer/types.ts
@@ -1,6 +1,22 @@
 // Copyright 2020-2024 SubQuery Pte Ltd authors & contributors
 // SPDX-License-Identifier: GPL-3.0
 
-import { AlgorandBlock } from '@subql/types-algorand';
+import { AlgorandBlock, AlgorandTransaction } from '@subql/types-algorand';
 
 export type BlockContent = AlgorandBlock;
+
+export function getBlockSize(block: BlockContent): number {
+  return (
+    block.transactions?.reduce(
+      (acc, tx) => acc + countInnerTransactions(tx),
+      0,
+    ) ?? 0
+  );
+}
+
+function countInnerTransactions(tx: AlgorandTransaction): number {
+  return (
+    tx.innerTxns?.reduce((acc, itx) => acc + countInnerTransactions(itx), 1) ??
+    1
+  );
+}
